108a51343b437bdd0e8b8d309eab631de00d6283
Philip Johnson <johnson@hawaii.edu>
2023-01-10 12:33:19 -1000

    checkpoint

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# flutter_firebase_auth_example
+# Flutter Firebase Auth Example
+
+This is a simplified version of the example app provided with the [firebase\_ui\_auth](https://github.com/firebase/flutterfire/tree/master/packages/firebase_ui_auth) package.
+
+This app only provides code to support email signin.
+
+This code illustrates the basic design pattern for the Firebase_ui_auth package. This package provides some prebuilt "screens" (widgets): [SignInScreen], [EmailVerificationScreen], [ForgotPasswordScreen], and [ProfileScreen]. You can customize the presentation of these widgets to follow your design theme. 
+
+Each of these screens take an "actions" parameter to which you can supply an [AuthStateChangeAction] in order to control what happens when a particular state in the authorization workflow occurs. For example, when the user successfully signs in with a verified email address, you probably want them to go to some sort of "home" page. The example app shows how you can use actions to navigate to various pages based upon the state of authorization. 
+
+## Screenshots
+
+###  Signin and registration
+
+<p style="text-align: center">
+  <img src="./README-screenshots/splash.png" width="45%">
+&nbsp; &nbsp; 
+  <img src="./README-screenshots/signin.png" width="45%">
+</p>
+
+## Installation
+
+Download this source code.
+
+Follow the instructions in [Firestore Setup Cheat Sheet](https://courses.ics.hawaii.edu/mobile-application-development/morea/data/reading-firestore-setup-cheat-sheet.html) to setup a Firestore database for this app.
+
+Run the app.
---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+// Copyright 2022, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'decorations.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseUIAuth.configureProviders([EmailAuthProvider()]);
+  runApp(const FirebaseAuthUIExample());
+}
+
+class FirebaseAuthUIExample extends StatelessWidget {
+  const FirebaseAuthUIExample({Key? key}) : super(key: key);
+
+  String get initialRoute {
+    final auth = FirebaseAuth.instance;
+    if (auth.currentUser == null || auth.currentUser!.isAnonymous) {
+      return '/';
+    }
+    if (!auth.currentUser!.emailVerified && auth.currentUser!.email != null) {
+      return '/verify-email';
+    }
+    return '/profile';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        visualDensity: VisualDensity.standard,
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
+        ),
+      ),
+      initialRoute: initialRoute,
+      routes: {
+        '/': (context) {
+          return SignInScreen(
+            actions: [
+              ForgotPasswordAction((context, email) {
+                Navigator.pushNamed(
+                  context,
+                  '/forgot-password',
+                  arguments: {'email': email},
+                );
+              }),
+              AuthStateChangeAction<SignedIn>((context, state) {
+                if (!state.user!.emailVerified) {
+                  Navigator.pushNamed(context, '/verify-email');
+                } else {
+                  Navigator.pushReplacementNamed(context, '/profile');
+                }
+              }),
+              AuthStateChangeAction<UserCreated>((context, state) {
+                if (!state.credential.user!.emailVerified) {
+                  Navigator.pushNamed(context, '/verify-email');
+                } else {
+                  Navigator.pushReplacementNamed(context, '/profile');
+                }
+              }),
+              AuthStateChangeAction<CredentialLinked>((context, state) {
+                if (!state.user.emailVerified) {
+                  Navigator.pushNamed(context, '/verify-email');
+                } else {
+                  Navigator.pushReplacementNamed(context, '/profile');
+                }
+              }),
+            ],
+            styles: const {
+              EmailFormStyle(signInButtonVariant: ButtonVariant.filled),
+            },
+            headerBuilder: headerImage('assets/images/flutterfire_logo.png'),
+            sideBuilder: sideImage('assets/images/flutterfire_logo.png'),
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  action == AuthAction.signIn
+                      ? 'Welcome to Firebase UI! Please sign in to continue.'
+                      : 'Welcome to Firebase UI! Please create an account to continue',
+                ),
+              );
+            },
+            footerBuilder: (context, action) {
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text(
+                    action == AuthAction.signIn
+                        ? 'By signing in, you agree to our terms and conditions.'
+                        : 'By registering, you agree to our terms and conditions.',
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        '/verify-email': (context) {
+          return EmailVerificationScreen(
+            headerBuilder: headerIcon(Icons.verified),
+            sideBuilder: sideIcon(Icons.verified),
+            actions: [
+              EmailVerifiedAction(() {
+                Navigator.pushReplacementNamed(context, '/profile');
+              }),
+              AuthCancelledAction((context) {
+                FirebaseUIAuth.signOut(context: context);
+                Navigator.pushReplacementNamed(context, '/');
+              }),
+            ],
+          );
+        },
+        '/forgot-password': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments
+              as Map<String, dynamic>?;
+
+          return ForgotPasswordScreen(
+            email: arguments?['email'],
+            headerMaxExtent: 200,
+            headerBuilder: headerIcon(Icons.lock),
+            sideBuilder: sideIcon(Icons.lock),
+          );
+        },
+        '/profile': (context) {
+          return ProfileScreen(
+            actions: [
+              SignedOutAction((context) {
+                Navigator.pushReplacementNamed(context, '/');
+              }),
+            ],
+          );
+        },
+      },
+      title: 'Firebase Email Authorization Demo',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
